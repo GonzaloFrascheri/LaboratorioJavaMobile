ed591d2bbe626eb2c39519605726df74d4567188
GonzaloFrascheri <gfrascheri@Gonzalo.localdomain>
2023-05-04 19:12:04 -0300

    Cambios menores

--- app/src/main/java/com/example/cargauymobile/MainActivity.java
+++ app/src/main/java/com/example/cargauymobile/MainActivity.java
@@ -11,4 +11,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
     }
+
+
+    /**
+     * Empieza el LoginWeb - Ejemplo profesor Fererico, clase Android 2022
+     *
+     * public void LoginWeb(View v){
+     *     String url = "";
+     *     Intent i = new Intent(Intent.ACTION_VIEW);
+     *     i.setData(Uri.parse(url));
+     *     startActivity(i);
+     * }
+     * */
+
+
+
+
 }
