5bfbf98ff30810375e3f7fdea73fa705e6f07543
GonzaloFrascheri <gfrascheri@Gonzalo.localdomain>
2023-05-06 22:01:10 -0300

    activity_log_in definitivo y cambios menores

--- app/src/main/java/com/example/cargauymobile/MainActivity.java
+++ app/src/main/java/com/example/cargauymobile/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cargauymobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
     }
-
 
     /**
      * Empieza el LoginWeb - Ejemplo profesor Federico, clase Android 2022
